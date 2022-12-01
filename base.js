5b545d6a30f7161bbd29883445f59b28438649ff
Luis Vasquez <playerluis159@gmail.com>
2022-12-01 00:29:54 -0500

    Ready

--- base.js
+++ base.js
@@ -1,3 +1,16 @@
+var x = document.createElement("link");
+x.setAttribute("rel", "stylesheet");
+x.setAttribute("type", "text/css");
+x.setAttribute("href", "custom_styles/transtions.css");
+var y = document.createElement("link");
+y.setAttribute("rel", "stylesheet");
+y.setAttribute("type", "text/css");
+y.setAttribute("href", "node_modules/bootstrap/dist/css/bootstrap.min.css");
+
+document.head.appendChild(x);
+document.head.appendChild(y);
+
+
 var header = document.createElement("header");
 var container = document.createElement("div");
 var div = document.createElement("div");
@@ -8,11 +21,14 @@ var formSearch = document.createElement("form");
 var searchInput = document.createElement("input");
 var buttonContainer = document.createElement("div");
 var searchButton = document.createElement("button");
-header.classList.add("p-3", "bg-dark", "text-white");
+
+header.classList.add("mx-1", "mt-1", "p-3", "bg-dark", "text-white");
+header.style.borderRadius = "25px"
+
 container.classList.add("container");
 div.classList.add("d-flex", "flex-wrap", "align-items-center", "justify-content-center", "justify-content-lg-start");
 logo.srcset = "resources/Logo_ESPE.png";
-logo.style.width = "100px"
+logo.style.width = "90px"
 logo.alt = "ESPE-NEWS";
 logoLink.classList.add("d-flex", "align-items-", "mb-2", "mb-lg-0", "text-white", "text-decoration-none");
 logoLink.href = "index.html";
@@ -29,15 +45,12 @@ searchButton.type = "button";
 searchButton.textContent = "Buscar"
 logoLink.appendChild(logo);
 
-
-
-addLinkToNav("INICIO", "#");
-addLinkToNav("NOTICIAS", "#");
-addLinkToNav("AMERICA LATINA", "#");
-addLinkToNav("INTERNACIONAL", "#");
-addLinkToNav("DEPORTES", "#");
-
-
+addLinkToNav("Inicio", "index.html");
+addLinkToNav("Noticias", "noticias.html");
+addLinkToNav("Seguridad", "seguridad.html");
+addLinkToNav("Politica", "politica.html");
+addLinkToNav("Deportes", "deportes.html");
+addLinkToNav("acerca_de", "acerca_de.html");
 
 formSearch.appendChild(searchInput);
 buttonContainer.appendChild(searchButton);
@@ -47,11 +60,14 @@ div.appendChild(formSearch);
 div.appendChild(buttonContainer);
 container.appendChild(div);
 header.appendChild(container);
-document.body.appendChild(header);
+document.body.prepend(header);
+
 
 console.log("a");
 function addLinkToNav(title, href) {
     var newLink = document.createElement("li");
+    newLink.classList.add("to-gray-hover");
+    newLink.style.borderRadius = "15px"
     var a = document.createElement("a");
     a.classList.add("nav-link", "px-3", "text-white");
     a.textContent = title;
