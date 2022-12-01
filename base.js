1718098a2b5a0438f2a69d343ecd212f5233abbc
Luis Vasquez <playerluis159@gmail.com>
2022-12-01 02:23:44 -0500

    Fin del index

--- base.js
+++ base.js
@@ -51,6 +51,10 @@ footer.innerHTML = `
   </footer>
 </div>
 `;
+
+
+
+
 footer.classList.add("bg-dark", "text-center", "text-white");
 footer.style.borderRadius = "25px";
 header.classList.add("mx-1", "mt-1", "p-3", "bg-dark", "text-white");
