654689475a25b00e10e388226b7272631caeada1
KevinStop <84851140+KevinStop@users.noreply.github.com>
2023-03-06 22:18:23 -0500

    Php acerca_de
    Cambio a php

--- acerca_de.php
+++ acerca_de.php
@@ -10,50 +10,21 @@
     <!--jQuery-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
-    <script src="node_modules/jquery-color/dist/jquery.color.min.js"></script>
     <title>Acerca de</title>
     <style>
         label.error{
             color: hsl(0, 74%, 64%);
         }
     </style>
-    <script>
-        $(() => {
-            $(".btn").on(
-                {
-                    mouseenter: function () {
-                        $(this).animate({ transform: 0.5 },
-                            {
-                                duration: 300, easing: 'linear',
-                                step: function (now, fx) {
-                                    $(this).css('transform', 'scale(' + (1 + now) + ')')
-                                }
-                            })
-    
-                    },
-                    mouseleave: function () {
-                        $(this).finish().animate({ transform: 0.5 },
-                            {
-                                duration: 300, easing: 'linear',
-                                step: function (now, fx) {
-                                    $(this).css('transform', 'scale(' + (1.5 - now) + ')')
-                                }
-                            })
-    
-                    }
-                }
-            )
-        })
-    </script>
 </head>
 
 <body>
     <div class="container">
-        <hr style="border:0px; border-top: 5px double #000;">
-        <h2 class="text-bg-dark text-center" id="demo">NOTICIAS ESPE</h2>
-        <hr style="border:0px; border-top: 5px double #000;">
-
-        <p style="text-align: justify;">
+        <h2>
+            <center>ESPE NEWS</center>
+        </h2>
+        <hr>
+        <p>
             Somos una cadena universitaria de noticias que aborda la actualidad preferentemente a través
             de noticias y el despliegue de medios de producción humanos y técnicos. El reportaje es un género que
             engloba
@@ -65,14 +36,13 @@
             y espacio reales.<br>
             <br>
             <hr>
-            <img src="https://uar.espe.edu.ec/wp-content/uploads/2019/11/ESPE1.png" class="border border-5 w-100 mb-4" alt="">
         <div class="site-section site-block-appointment">
             <div class="container">
                 <div class="row">
 
                     <div class="col-lg-6 mb-5 pl-lg-5 order-1 order-lg-2">
                         <div class="pl-lg-5 ml-lg-5">
-                            <h2 id="contactanos" class="site-heading text-black"><strong>! Infórmate !</strong> </h2>
+                            <h2 id="contactanos" class="site-heading text-black"><strong>!Informate !</strong> </h2>
 
                             <p class="lead text-black"> <br>¡OBTEN MAS INFORMACIÓN!
                             </p>
@@ -82,15 +52,15 @@
                             </p>
                             <center>
                                 <img src="https://www.espe.edu.ec/wp-content/uploads/2022/01/ESPEtransparente.png"
-                                    class="slide-element-inverse img-fluid" alt="..." style="width: 800px;">
+                                    class="img-fluid" alt="..." style="width: 800px;">
                             </center>
                         </div>
                     </div>
 
-                    <div class="slide-element col-lg-6 order-2 order-lg-1 rounded-3" style="background-color: rgb(44, 44, 44);">
+                    <div class="col-lg-6 order-2 order-lg-1 rounded-3" style="background-color: rgb(44, 44, 44);">
 
 
-                        <form name="contacto" action="#" method="POST" class="p-5 bg-rgb(44, 44, 44)">
+                        <form name="contacto" action="acerca_de.php" method="POST" class="p-5 bg-rgb(44, 44, 44)">
 
                             <div class="row form-group">
                                 <div class="col-md-6 mb-3 mb-md-0" style="padding: 5px; color: white;">
@@ -110,7 +80,7 @@
                                         placeholder="Telefono Celular">
                                 </div>
                                 <div class="col-md-6" style="padding: 5px; color: white;">
-                                    <label class="font-weight-bold" for="email">Correo Electrónico</label>
+                                    <label class="font-weight-bold" for="email">Correo Electronico</label>
                                     <input type="email" id="email" name="email" class="form-control" placeholder="Correo">
                                 </div>
                             </div>
@@ -123,7 +93,7 @@
                             </div>
 
                             <div class="row form-group">
-                                <div class="col-md-12" style="padding: 7px">
+                                <div class="col-md-12">
                                     <input type="submit" value="Enviar" class="btn"
                                         style="background: white; color: black;">
                                 </div>
@@ -187,9 +157,9 @@
                     required: "Por favor, introduzca su apellido",
                 },
                 cell: {
-                    required: "Por favor, introduzca su número de telefono",
-                    minlength: "Ingrese 10 dígitos",
-                    maxlength: "Ingrese 10 dígitos"
+                    required: "Por favor, introduzca su numero de telefono",
+                    minlength: "Ingrese 10 digitos",
+                    maxlength: "Ingrese 10 digitos"
                 },
                 email: {
                     required: "Por favor, introduzca una dirección de correo",
@@ -202,3 +172,43 @@
         });
     });
 </script>
+
+
+<?php
+$datos = array();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['fname'])) {
+        $fname = $_POST['fname'];
+        $datos['fname'] = $fname;
+    }
+    if (isset($_POST['lname'])) {
+        $lname = $_POST['lname'];
+        $datos['lname'] = $lname;
+    }
+    if (isset($_POST['cell'])) {
+        $cell = $_POST['cell'];
+        $datos['cell'] = $cell;
+    }
+    if (isset($_POST['email'])) {
+        $email = $_POST['email'];
+        $datos['email'] = $email;
+    }
+    if (isset($_POST['note'])) {
+        $note = $_POST['note'];
+        $datos['note'] = $note;
+    }
+}
+
+// Concatenar los datos en una cadena de texto
+$datos = "Nombre: $fname\nApellido: $lname\nTelefono: $cell\nCorreo Electronico: $email\nMensaje: $note\n\n";
+
+// Abrir el archivo de texto plano
+$archivo = fopen("datosPrueba.txt", "a");
+
+// Escribir los datos en el archivo de texto plano
+fwrite($archivo, $datos);
+
+// Cerrar el archivo
+fclose($archivo);
+
+?>
